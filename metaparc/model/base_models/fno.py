1d7620eeee318e63de8ba6a12c8e8c24b6743761
Florian Wiesner <wiesnerflorian94@gmail.com>
2025-03-27 18:40:28 -0400

    use a fno class as wrapper

--- metaparc/model/base_models/fno.py
+++ metaparc/model/base_models/fno.py
@@ -1,18 +1,9 @@
 from neuralop.models import FNO
 
 
-def get_fno(
-    n_modes: int,
-    in_channels: int,
-    out_channels: int,
-    hidden_channels: int,
-    n_layers: int,
-) -> FNO:
-    return FNO(
-        n_modes=n_modes,
-        in_channels=in_channels,
-        out_channels=out_channels,
-        hidden_channels=hidden_channels,
-        n_layers=n_layers,
-        projection_channel_ratio=2,
-    )
+class FourierNeuralOperator(FNO):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, x):
+        return super().forward(x)
