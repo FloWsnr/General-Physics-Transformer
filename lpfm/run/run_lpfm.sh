9c8da2953b3be0cd637a34b390984cf3a9452087
Florian Wiesner <florian.wiesner@avt.rwth-aachen.de>
2025-04-15 23:11:46 +0200

    add mem per cpu

--- lpfm/run/run_lpfm.sh
+++ lpfm/run/run_lpfm.sh
@@ -12,7 +12,10 @@
 #SBATCH --nodes=1
 
 ### How many CPU cores to use
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=24
+
+### How much memory per core
+#SBATCH --mem-per-cpu=5200
 
 ### Mail notification configuration
 #SBATCH --mail-type=ALL
