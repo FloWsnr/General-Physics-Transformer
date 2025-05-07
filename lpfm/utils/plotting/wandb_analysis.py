36852e3c22573bb46cc1021a54d4d25e05c8ccad
Florian Wiesner <wiesnerflorian94@gmail.com>
2025-05-06 20:44:06 -0400

    refactor wandb plotting

--- lpfm/utils/plotting/wandb_analysis.py
+++ lpfm/utils/plotting/wandb_analysis.py
@@ -8,47 +8,53 @@ from lpfm.utils.plotting.base_plotter import BasePlotter
 dotenv.load_dotenv()
 
 
-class WandbPlotter:
+class WandbLoader:
     def __init__(self, run_ids: list[str]):
         self.api = wandb.Api()
         entity, project = "wsnr-florian", "Large-Physics-Foundation-Model"
         self.runs: list[Run] = self.api.runs(
             f"{entity}/{project}", {"$or": [{"name": run_id} for run_id in run_ids]}
         )
-        print(f"Found {len(self.runs)} runs")
-
-    def plot_training_loss(self):
-        # get losses from all runs\
-        keys = ["training-losses/NMSE", "training/total_samples_trained"]
+        keys = [
+            "training-losses/NMSE",
+            "training/total_samples_trained",
+            "training/total_batches_trained",
+        ]
         data = {}
         for run in self.runs:
             data[run.name] = run.history(keys=keys, pandas=True)
+        self.data = pd.concat(data, axis=1)
+        self.data.columns = pd.MultiIndex.from_tuples(self.data.columns)
 
-        # create a multiindex df from the data
-        df = pd.concat(data, axis=1)
-        df.columns = pd.MultiIndex.from_tuples(df.columns)
 
+class LossPlotter(WandbLoader):
+    def __init__(self, run_ids: list[str]):
+        super().__init__(run_ids)
+        print(f"Found {len(self.runs)} runs")
+        self.y_ticks = [0.0001, 0.01, 1]
+
+    def loss_over_samples(self):
         plotter = BasePlotter(
             color="white",
         )
 
         # get the max number of samples trained
         max_samples = (
-            df.xs("training/total_samples_trained", axis=1, level=1).max().max()
+            self.data.xs("training/total_samples_trained", axis=1, level=1).max().max()
         )
 
         plotter.setup_figure(
             x_ticks=[0, max_samples // 2, max_samples],
-            y_ticks=[0.001, 0.1, 10],
+            y_ticks=self.y_ticks,
             x_label="Samples Trained",
             y_label="NMSE",
             y_log=True,
         )
 
         # plot the data
-        for run_name in data.keys():
-            samples = df[run_name]["training/total_samples_trained"]
-            loss = df[run_name]["training-losses/NMSE"]
+        for run_name in self.data.columns.get_level_values(0):
+            samples = self.data[run_name]["training/total_samples_trained"]
+            loss = self.data[run_name]["training-losses/NMSE"]
             color = next(plotter.color_cycler)
             plotter.plot_data(
                 samples, loss, label=run_name, symbolstyle="", color=color
@@ -57,9 +63,40 @@ class WandbPlotter:
         plotter.legend(title="Models", loc="upper right")
         plotter.show_figure()
 
+    def loss_over_batches(self):
+        plotter = BasePlotter(
+            color="white",
+        )
+
+        # get the max number of batches trained
+        max_batches = (
+            self.data.xs("training/total_batches_trained", axis=1, level=1).max().max()
+        )
+
+        plotter.setup_figure(
+            x_ticks=[0, max_batches // 2, max_batches],
+            y_ticks=self.y_ticks,
+            x_label="Batches Trained",
+            y_label="NMSE",
+            y_log=True,
+        )
+
+        # plot the data
+        for run_name in self.data.columns.get_level_values(0):
+            batches = self.data[run_name]["training/total_batches_trained"]
+            loss = self.data[run_name]["training-losses/NMSE"]
+            color = next(plotter.color_cycler)
+            plotter.plot_data(
+                batches, loss, label=run_name, symbolstyle="", color=color
+            )
+
+        plotter.legend(title="Models", loc="upper right")
+        plotter.show_figure()
+
 
 if __name__ == "__main__":
-    plotter = WandbPlotter(
+    plotter = LossPlotter(
         ["ti-cyl-sym-flow-0001", "ti-cyl-sym-flow-0001b", "ti-cyl-sym-flow-0001c"]
     )
-    plotter.plot_training_loss()
+    plotter.loss_over_samples()
+    plotter.loss_over_batches()
