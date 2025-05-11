c608c681f7c4519acff9c94c2e6a48505ef993e7
Florian Wiesner <wiesnerflorian94@gmail.com>
2025-05-11 14:37:28 -0400

    remove old base models which are not needed

--- lpfm/model/base_models/fno.py
+++
@@ -1,9 +0,0 @@
-from neuralop.models import FNO
-
-
-class FourierNeuralOperator(FNO):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def forward(self, x):
-        return super().forward(x)
