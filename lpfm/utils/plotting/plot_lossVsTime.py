c157c1611ccb54a550257dc5a0fa4af61d84c4a0
Florian Wiesner <wiesnerflorian94@gmail.com>
2025-05-29 14:39:20 -0400

    improve plotting

--- lpfm/utils/plotting/plot_lossVsTime.py
+++ lpfm/utils/plotting/plot_lossVsTime.py
@@ -17,21 +17,27 @@ class LossVsTimePlotter(BasePlotter):
         self.setup_figure(
             x_ticks=x_ticks,
             y_ticks=y_ticks,
-            x_label="Time",
-            y_label="NMSE",
+            x_label="Time steps",
+            y_label="MSE",
             y_log=y_log,
         )
 
-    def plot(self, mean_loss: torch.Tensor, std_loss: torch.Tensor):
-        mean_loss = mean_loss.cpu().numpy()
-        std_loss = std_loss.cpu().numpy()
+    def plot(
+        self,
+        mean_loss: torch.Tensor | np.ndarray,
+        std_loss: torch.Tensor | np.ndarray | None = None,
+    ):
+        if isinstance(mean_loss, torch.Tensor):
+            mean_loss = mean_loss.cpu().numpy()
+        if isinstance(std_loss, torch.Tensor):
+            std_loss = std_loss.cpu().numpy()
+
         for i, (channel, params) in enumerate(self.field_params.items()):
             color = params["color"]
             symbol = params["symbol"]
 
             time = np.arange(mean_loss.shape[0])
             mean_loss_channel = mean_loss[:, i]
-            std_loss_channel = std_loss[:, i]
 
             # plot mean loss
 
@@ -40,12 +46,14 @@ class LossVsTimePlotter(BasePlotter):
                 y_data=mean_loss_channel,
                 color=color,
                 label=channel,
-                symbolstyle=symbol,
+                symbolstyle="None",
                 markerstep=50,
             )
-            self.plot_error_region(
-                x_data=time,
-                y_data=mean_loss_channel,
-                y_err=std_loss_channel,
-                color=color,
-            )
+            if std_loss is not None:
+                std_loss_channel = std_loss[:, i]
+                self.plot_error_region(
+                    x_data=time,
+                    y_data=mean_loss_channel,
+                    y_err=std_loss_channel,
+                    color=color,
+                )
